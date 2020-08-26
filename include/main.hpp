d20e248993099fbc34dd184b46ca954ed9bfb605
kb10uy <kb10uy@kb10uy.org>
2020-08-26 22:10:41 +0900

    生理

---
+++ include/main.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Seeed_FS.h>
+#include <Seeed_SD.h>
+#include <lua.hpp>
+
+void setup();
+void loop();
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+#include "main.hpp"
 
 void setup() {
 }
